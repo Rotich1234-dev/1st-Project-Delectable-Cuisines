b7b3ee593b4ef94a95b18d9e2df03dfaa8b6901c
Rotich <jane.rotich@student.moringaschool.com>
2024-01-19 17:18:27 +0300

    fix:fetch data from two APIs

--- script.js
+++ script.js
@@ -1,30 +1,24 @@
 // Execute code when DOM is fully loaded
-document.addEventListener("DOMContentLoaded", function(){
-    // spoonacular key and queries predefined
+document.addEventListener("DOMContentLoaded", function () {
     const apiKey = "9aeb052b09c44418aa30cc35a475f19f";
-    const queries = ["chicken", "burger", "snickers"]; 
-     // API for the first set of menu items
-     const apiUrl1 = `https://api.spoonacular.com/food/menuItems/search?apiKey=${apiKey}&query=${queries.join("&query=")}`;
+    const queries = ["chicken", "burger", "snickers"];
 
-     // API for the second set of menu items
-     const apiUrl2 = `https://api.spoonacular.com/food/menuItems/search?apiKey=${apiKey}&query=rice&query=chicken`;
-    //  to make concurrent requests to both APIs
+    const apiUrl1 = `https://api.spoonacular.com/food/menuItems/search?apiKey=${apiKey}&query=${queries.join("&query=")}`;
+    const apiUrl2 = `https://api.spoonacular.com/food/menuItems/search?apiKey=${apiKey}&query=${queries.join("&query=")}`;
 
     Promise.all([fetch(apiUrl1), fetch(apiUrl2)])
-    .then(responses => Promise.all(responses.map(response => response.json())))
-    .then(dataArray => {
-        // Get the container where menu items will be displayed
-        const menuContainer = document.getElementById("menu");
-        // Iterate through the data and create menu items dynamically
+        .then(responses => Promise.all(responses.map(response => response.json())))
+        .then(dataArray => {
+            const menuContainer = document.getElementById("menu");
 
-        dataArray.forEach(data => {
-            data.menuItems.forEach(item => {
-                const menuItemDiv = createMenuItem(item);
-                menuContainer.appendChild(menuItemDiv);
+            dataArray.forEach(data => {
+                data.menuItems.forEach(item => {
+                    const menuItemDiv = createMenuItems(item); // Fix the function name
+                    menuContainer.appendChild(menuItemDiv);
+                });
             });
-        });
-    })
-    .catch(error => console.error("Error fetching menu items:", error));
+        })
+        .catch(error => console.error("Error fetching menu items:", error));
     // Create a div for the menu item
     function createMenuItems(item){
         const menuItemDiv = document.createElement("div");
